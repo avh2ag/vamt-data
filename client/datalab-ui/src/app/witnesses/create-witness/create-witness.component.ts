d3389908c56803a9f36ea8c0c7a3d928bdb1140f
Alex Harasty <alex.harasty@thehumangeo.com>
2017-10-08 23:34:17 -0400

    component for creating witnesses with type created

--- client/datalab-ui/src/app/witnesses/create-witness/create-witness.component.ts
+++ client/datalab-ui/src/app/witnesses/create-witness/create-witness.component.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { WitnessService } from '../witness.service';
+import { MatSnackBar } from '@angular/material';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'create-witness',
   templateUrl: './create-witness.component.html',
   styleUrls: ['./create-witness.component.css']
 })
 export class CreateWitnessComponent implements OnInit {
-
-  constructor(private witnessService: WitnessService) { }
-
+  witnessForm: FormGroup;
+  constructor(private snackBar: MatSnackBar, private witnessService: WitnessService) { }
+  witnessTypes: ['Character', 'Defendant', 'Expert', 'Crying', 'Cop'];
   ngOnInit() {
-  	this.witnessService.createWitness("test", "character").subscribe(resp => {
-  		console.log(resp);
+  	this.onFormReset();
+  }
+
+  onFormReset() {
+  	this.witnessForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required]),
+      'type': new FormControl(null, [Validators.required]),
+    });
+  }
+
+  createWitness() {
+  	let witnessName = this.witnessForm.value.name;
+  	let witnessType = this.witnessForm.value.type;
+  	this.witnessService.createWitness(witnessName, witnessType).subscribe(resp => {
+  		let message = `${witnessName} created.`;
+  		this.snackBar.open(message, "", {duration: 2500});
+  		this.onFormReset();
   	}, err => {
   		console.log(err);
-  	});
+  		let message = `Error creating ${witnessName}`;
+  		this.snackBar.open(message, "", {duration: 2500});
+  	});  	
   }
 
 }
--- client/datalab-ui/src/app/witnesses/witnesses.module.ts
+++ client/datalab-ui/src/app/witnesses/witnesses.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WitnessService } from './witness.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateWitnessComponent } from './create-witness/create-witness.component';
+import { MatSnackBarModule, MatIconModule, MatButtonModule,
+MatFormFieldModule, MatInputModule, MatSelectModule,
+MatTooltipModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   imports: [
+  	FlexLayoutModule,
+  	MatSnackBarModule, MatIconModule, MatButtonModule,
+  	MatFormFieldModule, MatInputModule, MatSelectModule,
+  	FormsModule, ReactiveFormsModule, MatTooltipModule,
     CommonModule
   ],
   providers: [
