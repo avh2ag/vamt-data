995da6d6c7ec9bc490532fa2b53f860a4fc18cec
Alex Harasty <alex.harasty@thehumangeo.com>
2017-10-09 14:11:03 -0400

    add form validation for adding witness to list

--- client/datalab-ui/src/app/witnesses/select-witness/select-witness.component.ts
+++ client/datalab-ui/src/app/witnesses/select-witness/select-witness.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs/Observable';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/map';
 import { Witness } from '../../config/models';
@@ -11,12 +11,15 @@ import { WitnessService } from '../witness.service';
   styleUrls: ['./select-witness.component.css']
 })
 export class SelectWitnessComponent implements OnInit {
+  @Input() outputList: Array<Witness>;	
   witnessSubscription;
   witnessControl = new FormControl();
   options: Array<Witness> = [];
   filteredOptions: Observable<Witness[]>;
+
   constructor(private witnessService: WitnessService) { }
   ngOnInit() {
+  	console.log(this.outputList);
   	this.options = this.witnessService.loadedWitnesses;
   	this.witnessSubscription = this.witnessService.notifyDataChanged.subscribe((witnesses: Array<Witness>) => {
   		this.options = witnesses;
@@ -27,6 +30,7 @@ export class SelectWitnessComponent implements OnInit {
         .map(name => name ? this.filter(name) : this.options.slice());
   }
 
+
   filter(name: string): Witness[] {
     return this.options.filter(option =>
       option.witness_name.toLowerCase().indexOf(name.toLowerCase()) === 0);
@@ -34,6 +38,10 @@ export class SelectWitnessComponent implements OnInit {
 
   displayFn(witness: Witness): string {
     return witness ? witness.witness_name : "";
+  }
+
+  addWitness() {
+  	console.log(this.witnessControl.value);
   }
 
 }
