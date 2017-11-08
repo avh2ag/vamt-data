3692b2ecbcd53afd1234357bc02be72923e98f19
Alex Harasty <alex.harasty@thehumangeo.com>
2017-11-07 22:36:58 -0500

    bind the show create tournament dialog

--- client/datalab-ui/src/app/tournaments/tournament-list/tournament-list.component.ts
+++ client/datalab-ui/src/app/tournaments/tournament-list/tournament-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { CreateTournamentComponent } from '../create-tournament/create-tournament.component';
 import { Tournament } from '../../config/models';
 @Component({
   selector: 'app-tournament-list',
@@ -9,9 +11,18 @@ export class TournamentListComponent implements OnInit {
   public loading: boolean = false;
   public previewVisible: boolean = true;
   public allTournaments: Array<Tournament> = [];
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
+  showCreateDialog() {
+    let dialogRef = this.dialog.open(CreateTournamentComponent, {
+      width: '80%',
+      height: '80%'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });  
+  }
 }
